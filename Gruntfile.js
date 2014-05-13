2752732ef6f55aa5f57bf67455ceb8a0b9251ac3
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-13 13:15:53 -0400

    Added Tests in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,7 @@
+var shell = require('shelljs');
+
 module.exports = function (grunt) {
-    grunt.initConfig();
+    grunt.registerTask("test", function () {
+        shell.exec("node src/main.js --testdir=tests/ --json=test-report.json");
+    });
 };
