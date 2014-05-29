c208b7f84ecc7f7d4202b8e87c6174748a76531d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-29 10:25:22 -0400

    NPM Test Fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,12 @@
 var shell = require('shelljs');
+var semver = require("semver");
 
 module.exports = function (grunt) {
-    grunt.registerTask("test", function () {
+    grunt.registerTask("run-tests", function () {
         shell.exec("node src/main.js --testdir=tests/ --json=test-report.json");
     });
+    
+    grunt.registerTask("test", [ "check-version", "run-tests" ]);
 
     grunt.registerTask("update", function () {
         shell.exec("npm update");
@@ -11,5 +14,22 @@ module.exports = function (grunt) {
 
     grunt.registerTask("publish", function () {
         shell.exec("npm publish");
+    });
+    
+    grunt.registerTask("check-version", function () {
+        var pkginfo = grunt.file.readJSON("package.json");
+        if (!semver.valid(pkginfo.version)) {
+            grunt.log.error("Specified Version '" + pkginfo.version + "' is not valid.");
+            return false;
+        }
+    });
+    
+    grunt.registerTask("update-version", function () {
+        var pkginfo = grunt.file.readJSON("package.json");
+        var current = pkginfo.version;
+        var next = semver.inc(current, "patch");
+        grunt.log.write("Updating Version to v" + next);
+        pkginfo.version = next;
+        grunt.file.write("package.json", JSON.stringify(pkginfo, null, 4));
     });
 };
